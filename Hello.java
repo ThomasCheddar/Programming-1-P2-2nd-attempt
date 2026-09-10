b85cbd7d6910d99cab155291fbbac7beeaf6b4e8
Thomas <thomasomg1@outlook.com>
2026-09-10 08:51:10 -0400

    Added a little message print :D

---
+++ Hello.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Hello here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Hello
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Hello
+     */
+    public Hello()
+    {
+        x = 0;
+    }
+    
+    public void sampleMethod(){
+        System.out.println("Hello world");
+    }
+}
